9146a6ea4142d33c27625047efdc3d1b26a5dcbe
XenKos <158058743+XenKos@users.noreply.github.com>
2024-01-30 18:06:26 +0100

    Update data.py

--- data.py
+++ data.py
@@ -1,10 +1,9 @@
 from datetime import datetime, timedelta
 
-def get_days_from_today():
+def get_days_from_today(date):
     
-    date=input("Input the date: ") 
-     
     try:
+        date=input("Input the date: ") 
         date=datetime.strptime(date, "%Y-%m-%d")
     except ValueError:
         print ("Incorrect date format. Input the date in format YYYY-MM-DD.")
@@ -12,10 +11,12 @@ def get_days_from_today():
 
     else:
         current_date=datetime.today()
-        delta = date  - current_date
-        return delta.days
+        days_difference =date  - current_date
+        return days_difference.days
     
-days_difference = get_days_from_today()
+date = None
+days_difference=get_days_from_today(date)
 
 if days_difference is not None:
+
     print("Get days from today: ", days_difference)
